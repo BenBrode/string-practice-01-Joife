158d22cf4b418881f6f6217c545ca421d2cb7657
Jack Qiu <qiuguangliang@Jacks-Macbook-Pro.local>
2018-11-30 20:40:09 +0800

    Jack's string practice code

---
+++ 20181130-Jack/Phrase.java
@@ -0,0 +1,25 @@
+//20181130 Jack String practice 01
+public class Phrase{
+    private String currentPhrase;
+
+    public Phrase(String p){
+        currentPhrase = p;
+    }
+    public int findNthOccurence(String str, int n){
+        int index = 0;
+        return index;
+    }
+    
+    //part a
+    public void replaceNthOccurence(String str, int n, String repl){
+        
+    }
+
+    //part b
+    public int findLastOccurence(String str){
+
+    }
+    public String toString(){
+        return currentPhrase;
+    }
+}
